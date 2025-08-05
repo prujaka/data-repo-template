d85c98d1cd4701804be2a1b3045504cf0214b61d
Sergey Tkachenko <sergeyprujina@gmail.com>
2025-08-05 16:25:25 +0200

    refactor: config loading with pathlib

--- package/constants.py
+++ package/constants.py
@@ -1,8 +1,12 @@
 from configparser import ConfigParser
+from pathlib import Path
 
+CONFIG_NAME = "config.ini"
+CONFIG_PATH = Path(__file__).resolve().parent.parent / CONFIG_NAME
 
 config = ConfigParser()
-config.read('../config.ini')
+config.read(CONFIG_PATH)
 
 API_KEY = config['secrets']['api_key']
 DATABASE_ID = config['databases']['database_id']
+
